da5470253e75459f863cc76d69453f2033cd7442
HX-ASandoval2 <auri.sandoval@gmail.com>
2023-02-10 17:18:28 -0300

    mi tercer commit

--- demo.js
+++ demo.js
@@ -180,18 +180,18 @@ var autos = ["Ford", "Chevrolet", "Toyota", "Tesla"];
 
 // console.log(usuario)
 
-var alumna = {
-    nombre:"Nohe",
-    apellido:"Martinez",
-    etapa:"prepcourse full stack",
-    online:true,
-    saludo:function(){
-        // return 'Mi nombre es ' + this.nombre + ' ' + this.apellido + ' y estoy en la etapa de ' + this.etapa + ' y estoy' + this.online
-        return `Mi nombre es ${this.nombre} ${this.apellido} y estoy en la etapa de ${this.etapa} y estoy ${this.online}`
-    }
-}
+// var alumna = {
+//     nombre:"Nohe",
+//     apellido:"Martinez",
+//     etapa:"prepcourse full stack",
+//     online:true,
+//     saludo:function(){
+//         // return 'Mi nombre es ' + this.nombre + ' ' + this.apellido + ' y estoy en la etapa de ' + this.etapa + ' y estoy' + this.online
+//         return `Mi nombre es ${this.nombre} ${this.apellido} y estoy en la etapa de ${this.etapa} y estoy ${this.online}`
+//     }
+// }
 
-console.log(alumna.saludo())
+// console.log(alumna.saludo())
 // console.log(alumna.nombre)
 
 // console.log(Object.keys(alumna))
@@ -209,3 +209,160 @@ console.log(alumna.saludo())
 
 
     
+// function Gato(nombre){
+//     this.nombre = nombre;
+//     this.maullar = function(){
+//         return 'Mi nombre es ' + this.nombre + '...Meow'
+//     }
+// }
+
+// //instanciemos
+// var mindy = new Gato('Mindy')
+// var blacky = new Gato('Blacky')
+// var coco = new Gato('Coco')
+
+// console.log(mindy.maullar())
+// console.log(blacky.maullar())
+// console.log(coco.maullar())
+
+// class Gato{
+//     constructor(nombre){
+//         this.nombre = nombre;
+//     }
+//     maullar(){
+//         return 'Mi nombre es ' + this.nombre + '...Meow'
+//     }
+// }
+
+// var mindy = new Gato('Mindy')
+// var blacky = new Gato('Blacky')
+// var coco = new Gato('Coco')
+
+// console.log(mindy.maullar())
+// console.log(blacky.maullar())
+// console.log(coco.maullar())
+
+
+// function Usuario(nombre,email){
+//     this.nombre = nombre;
+//     this.email = email;
+// }
+
+// Usuario.prototype.presentacion = function(){
+//     return "Mi nombre es " + this.nombre + ', mi email es ' + this.email
+// }
+
+// var nohe = new Usuario('Nohe','nohe@gmail.com')
+// var harane = new Usuario('Harane', 'harane@mail.com')
+
+// nohe.presentacion();
+// harane.presentacion();
+
+// var obj = {}
+
+// var obj = Object.create({})
+
+// console.log(obj);
+
+// var academia = {
+//     nombre:"Henry"
+// }
+
+// var alumna = Object.create( {name:{value:"Harane"}})
+
+// console.log(alumna.name);
+
+// var obj = {}
+
+// obj.nombre= "Sheila"
+
+// Object.assign(obj,{nombre:"Nadina", apellido:"Gimenez"})
+// console.log(obj);
+
+// Array.prototype.mayorQueTres = function(){
+//     var arreglo = [];
+//     for(var i = 0 ; i < this.length ; i++){
+//         if(this[i] > 3){
+//             arreglo.push(this[i])
+//         }
+//     }
+
+//     return arreglo;
+// }
+
+
+// var arr = [1,2,3,4,6,9];
+
+// var nuevoArreglo = arr.mayorQueTres();
+
+// console.log(nuevoArreglo);
+
+// class Mascota {
+//     constructor(){
+//         this.mascota = []
+//     }
+// }
+
+// Mascota.prototype.agregarMascota = function(mascotica){
+//     this.mascota.push(mascotica)
+// }
+
+// var mascotas = new Mascota()
+
+// mascotas.agregarMascota('perro')
+// mascotas.agregarMascota('gato')
+
+// console.log(mascotas.mascota)
+
+
+class Persona {
+    constructor(nombre,edad){
+        this.nombre = nombre;
+        this.edad = edad
+    }
+
+    saludar(){
+        return 'hola mi nombre es ' + this.nombre + ' y tengo ' + this.edad + ' años.'
+    }
+}
+
+class Desarrolladora extends Persona{
+    constructor(nombre, edad, tecnologia){
+        super(nombre, edad)
+        this.tecnologia = tecnologia
+    }
+    codear(){
+        return 'soy ' + this.nombre + ' tengo ' + this.edad + ' años y estudio ' + this.tecnologia
+    }
+    
+}
+
+var nadina = new Persona('Nadina', 23)
+console.log(nadina.saludar());
+
+var desarrolladora = new Desarrolladora('Nadina', 23, "Javascript")
+
+console.log(desarrolladora.codear())
+
+
+
+// function decirHola(usuario){
+//     return 'Hola ' + usuario
+// }
+
+// function decirAdios(usuario){
+//     return 'Adiós ' + usuario
+// }
+
+// function crearSaludo(usuario, cb, cb2){
+//    return cb(usuario) + ' ' + cb2(usuario);
+    
+// }
+
+// var obj = {
+//     nombre:'Rosary',
+//     apellido:'Chiu'
+// }
+
+// console.log(crearSaludo("Harane",decirHola));
+// console.log(crearSaludo("Harane",decirAdios));
