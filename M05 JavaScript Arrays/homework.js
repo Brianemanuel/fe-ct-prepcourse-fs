8ab9e3acb1419fc6ef4ca2c88ed196328ecca8d8
brian almiron <almironbrianemanuel@gmail.com>
2024-06-11 14:12:50 -0300

    Echo hasta el ejercicio mesesDelAño

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -161,6 +161,16 @@ function mesesDelAño(array) {
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
+   var meses = [];
+   for(i = 0 ; i < array.length; i++){
+      if(array[i]=== "Enero" || array[i] === "Marzo" || array[i] === "Noviembre"){
+         meses.push(array[i]);
+      }
+    }
+   if(meses.length === 3){
+      return meses;
+   }
+   return "No se encontraron los meses pedidos"
 }
 
 function tablaDelSeis() {
